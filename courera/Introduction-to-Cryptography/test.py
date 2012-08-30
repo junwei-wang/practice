fe360cb40ea37d287da0be5bb401d08465e95ae1
Junwei Wang <wakemecn@gmail.com>
2012-08-30 12:52:23 +0800

    new file:   week1-solution.py 	new file:   week1-src.py 	modified:   ../../README

---
+++ courera/Introduction-to-Cryptography/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+#
+# Filename:      test.py
+# Author:        Junwei Wang(wakemecn@gmail.com)
+# Last Modified: 2012-08-30 10:33
+# Description:
+#
+#
+        
+target = "abc"
+print target.encode("hex")
---
+++ courera/Introduction-to-Cryptography/week1-src.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2
+#
+# Filename:      week1-src.py
+# Author:        Junwei Wang(wakemecn@gmail.com)
+# Last Modified: 2012-08-28 21:55
+# Description:
+#
+#
+import sys
+
+MSGS = ( ---  11 secret messages  --- )
+
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+def random(size=16):
+    return open("/dev/urandom").read(size)
+
+def encrypt(key, msg):
+    c = strxor(key, msg)
+    print
+    print c.encode('hex')
+    return c
+
+def main():
+    key = random(1024)
+    ciphertexts = [encrypt(key, msg) for msg in MSGS]
